ec4e7ea70ebc76e9c2be6db21e356cc591783226
Surejja Alibegovic <surejja.a18@gmail.com>
2024-05-13 12:21:22 +0200

    Initial commit

--- test/pageobjects/page.js
+++ test/pageobjects/page.js
@@ -1,15 +1,10 @@
 import { browser } from '@wdio/globals'
 
-/**
-* main page object containing all methods, selectors and functionality
-* that is shared across all page objects
-*/
 export default class Page {
     /**
-    * Opens a sub page of the page
-    * @param path path of the sub page (e.g. /path/to/page.html)
+    @param path 
     */
     open (path) {
-        return browser.url(`https://the-internet.herokuapp.com/${path}`)
+        return browser.url(`https://fakerestapi.azurewebsites.net/api/v1/Authors${path}`)
     }
 }
---
+++ test/specs/test.api.js
@@ -0,0 +1,9 @@
+import { expect } from '@wdio/globals'
+import requestsPage from '../pageobjects/requests.page.js'
+
+describe('API smoke test', () => {
+    it('', async () => {
+        
+    })
+})
+
